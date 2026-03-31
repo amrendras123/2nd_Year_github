86b6d47a564def395ffc9ac0cca7b2f782b6579b
amrendras123 <samrendra497@gmail.com>
2026-03-31 12:55:55 +0530

    31-03-2026

---
+++ Sec-AC/Lab-8/Minreversal.java
@@ -0,0 +1,19 @@
+import java.util.Stack;
+
+public class Minreversal {
+     public int minSwaps(String s) {
+         Stack<Character> st=new Stack<>();
+         for(int i=0;i<s.length();i++){
+            char ch=s.charAt(i);
+            if(st.isEmpty()||ch=='['){
+                st.push(ch);
+            }else if(ch==']'&&st.peek()=='['){
+              st.pop();  
+            }else{
+                st.push(ch);
+            }
+         }
+         int pair=st.size()/2;
+         return (pair+1)/2;
+    }
+}
---
+++ Sec-AC/Lab-8/minAdd.java
@@ -0,0 +1,19 @@
+import java.util.Stack;
+
+public class minAdd {
+
+      public int minAddToMakeValid(String s) {
+         Stack<Character> st=new Stack<>();
+         for(int i=0;i<s.length();i++){
+            char ch=s.charAt(i);
+            if(st.isEmpty()||ch=='('){
+                st.push(ch);
+            }else if(ch==')'&&st.peek()=='('){
+              st.pop();  
+            }else{
+                st.push(ch);
+            }
+         }
+         return st.size();
+    }
+}
