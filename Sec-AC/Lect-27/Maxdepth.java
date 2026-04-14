9382894cf300b400a23776846a7c8ccab5fe65ba
amrendras123 <samrendra497@gmail.com>
2026-04-14 10:45:16 +0530

    14-04-2026

---
+++ Sec-AC/Lect-27/Maxdepth.java
@@ -0,0 +1,20 @@
+
+
+class Maxdepth{
+    class TreeNode{
+        int val;
+        TreeNode left;
+        TreeNode right;
+        
+    };
+    public int maxDepth(TreeNode root) {
+
+        if (root == null) {
+            return 0;
+        }
+
+        int lh = maxDepth(root.left);
+        int rh = maxDepth(root.right);
+        return Math.max(lh, rh) + 1;
+    }
+}
---
+++ Sec-BB/Lect-24/MaxD.java
@@ -0,0 +1,13 @@
+public class MaxD {
+
+     public int maxDepth(TreeNode root) {
+        
+        if(root==null){
+            return 0;
+        }
+        int lh=maxDepth(root.left);
+        int rh=maxDepth(root.right);
+
+        return Math.max(lh,rh)+1;
+    }
+}
